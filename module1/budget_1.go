4e4bb1b622eadc5dc627f52bd2987d5b143d0843
Brad Schmidt <Brad.Schmidt@phoenix.edu>
2020-08-11 09:27:33 -0700

    Added structs Budget and Item

--- module1/budget_1.go
+++ module1/budget_1.go
@@ -1,5 +1,12 @@
 package module1
 
 // Budget stores budget information
-
+type Budget struct {
+	max float32
+	Items Item[]
+}
 // Item stores item information
+type Item struct {
+	Description string
+	Price float32
+}
