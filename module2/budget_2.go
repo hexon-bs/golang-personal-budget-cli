9d3480ec64c199a0e67bd3235176f4f2086eeb3e
Brad Schmidt <Brad.Schmidt@phoenix.edu>
2020-08-12 09:14:03 -0700

    Updated budget_2.go

--- module2/budget_2.go
+++ module2/budget_2.go
@@ -74,9 +74,9 @@ func (b *Budget) RemoveItem(description string) {
 // CreateBudget creates a new budget with a specified max
 func CreateBudget(month time.Month, max float32) (*Budget, error) {
 	var newBudget *Budget
-	if (len(reprot) >= 12 {
+	if (len(report)) >= 12 {
 		return nil, errReportIsFull
-	})
+	}
 	if _, hasEntry := report[month]; hasEntry {
 		return nil, errDuplicateEntry
 	}
@@ -87,7 +87,7 @@ func CreateBudget(month time.Month, max float32) (*Budget, error) {
 
 // GetBudget returns budget for given month
 func GetBudget(month time.Month) *Budget {
-	
+
 	return nil
 }
 
